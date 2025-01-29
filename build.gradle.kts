4746b1fd56e9f11e5e1d7b2b4ec1b2fc233b78ed
Teemo He <1440492700@qq.com>
2025-01-29 11:41:55 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.teemo"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("232.*")
+        untilBuild.set("299.*")
     }
 
     signPlugin {
